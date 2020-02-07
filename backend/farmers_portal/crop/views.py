e378d1d95441caeb28a26900c59810d8d4698753
Yash Soni <yashsoni77779@gmail.com>
2020-02-07 08:45:03 +0530

    Crops added

--- backend/farmers_portal/crop/views.py
+++ backend/farmers_portal/crop/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Crop
+from .serializers import CropSerializer
 
-# Create your views here.
+# @api_view(["GET"])
+# def CropListView(request):
+
+#     if request.method == "GET":
+#         crops = Crop.objects.all()
+#         serializer = CropSerializer(crops,many=True)
+#         return Response(serializer.data)
+    
+#     elif request.method == "POST":
+#         serializer = CropSerializer(data= requesyt.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+
+
+# def Crop
