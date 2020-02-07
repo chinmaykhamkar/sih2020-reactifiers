9df64ce947e3bc679a67a7317a1b02bbc9aff6a2
Chinmay Khamkar <khamkarchinmay4@gmail.com>
2020-02-07 10:01:24 +0530

    qwerty

--- frontend/light-bootstrap-dashboard-master/assets/js/index.js
+++ frontend/light-bootstrap-dashboard-master/assets/js/index.js
@@ -1,2 +1,22 @@
-var username = document.getElementById('login').value;
-var password = document.getElementById('password').value;
+function login() {
+    console.log('inside function');
+    var username = document.getElementById('login').value;
+    var password = document.getElementById('password').value;
+    axios.post('http://127.0.0.1:8000/user/farmer_login/', {
+            username: username,
+            password: password
+        })
+        .then(function(data) {
+            console.log(data.data.valid);
+            window.sessionStorage.setItem('check', data.data.name);
+            if (data.data.valid === 'true') {
+
+                window.location.href = 'dashboard.html';
+            } else {
+                alert('wrong passsword');
+                window.location.href = 'login.html';
+
+            }
+        })
+
+}
